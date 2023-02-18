9b75ae55ac614f6a20cb1b1f66c042066b708cf3
Mary Lueder <mlueder@gmail.com>
2023-02-18 12:01:18 -0500

    capitalize each word in a string

--- codewars.js
+++ codewars.js
@@ -5,3 +5,18 @@ function solution(str, ending) {
 }
 
 console.log(solution('abcdebc', 'bc'));
+
+
+//capitalize each word. Your task is to convert strings to how they would be written by Jaden Smith. 
+
+let str = "How can mirrors be real if our eyes aren't real";
+
+String.prototype.toJadenCase = function () {
+  const str = this.split(' ')
+  const sent = str.map(word => (word[0].toUpperCase() + word.slice(1)))
+  return sent.join(' ')
+};
+
+console.log(str.toJadenCase())
+
+
