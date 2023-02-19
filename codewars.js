d8e830046e3e1d1f2edcd62a7c04b32af28c97e0
Mary Lueder <mlueder@gmail.com>
2023-02-19 11:18:43 -0500

    series sum function

--- codewars.js
+++ codewars.js
@@ -20,3 +20,27 @@ String.prototype.toJadenCase = function () {
 console.log(str.toJadenCase())
 
 
+// Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+// Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+// Rules:
+// You need to round the answer to 2 decimal places and return it as String.
+
+// If the given value is 0 then it should return 0.00
+
+// You will only be given Natural Numbers as arguments.
+
+function SeriesSum(n) {
+  let sum = 0
+  let denominator = 1
+  for (let i = 0; i < n; i++) {
+    console.log("denom before ", denominator)
+    sum += 1/denominator
+    denominator = denominator + 3
+    console.log("denom after ", denominator)
+  }
+  sum = sum.toFixed(2)
+  return sum.toString()
+}
+
+console.log(SeriesSum(0));
