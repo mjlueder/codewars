9f66b1768bef6ecf54b419ab3270be212de87333
Mary Lueder <mlueder@gmail.com>
2023-02-20 10:22:47 -0500

    calculate liters

--- codewars.js
+++ codewars.js
@@ -56,3 +56,12 @@ function past(h, m, s){
 }
 
 // Alt:  return ((h*3600)+(m*60)+s)*1000;
+
+
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+function litres(time) {
+  return `liters = ${Math.floor(time*0.5)}`
+}
