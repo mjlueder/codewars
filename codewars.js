9d84aac4d36654df7646672889589d974f9f5a4f
Mary Lueder <mlueder@gmail.com>
2023-03-02 11:18:54 -0500

    add squarenum problem

--- codewars.js
+++ codewars.js
@@ -76,3 +76,16 @@ function litres(time) {
 const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
   return mpg*fuelLeft > distanceToPump
 };
+
+
+
+
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 
+
+
+function squareSum(numbers){
+
+}
+
