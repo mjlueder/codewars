74a5221b78e310f06cf838132948dfb3620a8552
Mary Lueder <mlueder@gmail.com>
2023-03-02 11:22:27 -0500

    solve sum of a series of squared numbers

--- codewars.js
+++ codewars.js
@@ -86,6 +86,10 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 
 
 function squareSum(numbers){
-
+  let sum = 0
+  numbers.forEach(number => {
+    sum += number*number
+  });
+  return sum
 }
 
