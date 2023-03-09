afe4d053f37d30d8291f7a4c63fbe2410f32d176
Mary Lueder <mlueder@gmail.com>
2023-03-09 11:37:34 -0500

    solve grade book

--- codewars.js
+++ codewars.js
@@ -167,5 +167,20 @@ function getAge(inputString){
 // Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
 
 function getGrade (s1, s2, s3) {
-  // Code here
+  let avg = (s1 + s2 + s3)/3
+  if (avg >= 90 ) {
+    return 'A'
+  } else if (avg >= 80 ) {
+    return 'B'
+  } else if (avg >= 70 ) {
+    return 'C'
+  } else if (avg >= 60 ) {
+    return 'D'
+  } else {
+    return 'F'
+  }
 }
+
+// console.log(getGrade(95,90,93));
+// console.log(getGrade(84,79,85));
+// console.log(getGrade(60,82,76));
