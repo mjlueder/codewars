e890a983b3755f3e0535e8d013384b466a29aecc
Mary Lueder <mlueder@gmail.com>
2023-03-11 10:51:06 -0500

    set up and solve number to reversed array of digits problem:

--- codewars.js
+++ codewars.js
@@ -201,3 +201,15 @@ function numberToString(num) {
 }
 
 
+// Convert number to reversed array of digits
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+// Example(Input => Output):
+// 35231 => [1,3,2,5,3]
+// 0 => [0]
+
+function digitize(n) {
+  return n.toString().split('').reverse().map(letter => (parseInt(letter)))
+}
+
+console.log(digitize(1234));
