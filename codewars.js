5d6df8737e40cb9a28c994623e20930c59af5991
Mary Lueder <mlueder@gmail.com>
2023-03-13 10:29:27 -0400

    solve number series sum

--- codewars.js
+++ codewars.js
@@ -230,5 +230,21 @@ function digitize(n) {
 // Your function should only return a number, not the explanation about how you get that number.
 
 function getSum(a, b) {
-  
+  if (a === b) return a
+  if ( a > b ) {
+    let sum = 0
+    for(let i = b; i <= a; i++){
+      sum += i
+    }
+    return sum
+  }
+  if ( a < b ) {
+    let sum = 0
+    for(let i = a; i <= b; i++){
+      sum += i
+    }
+    return sum
+  }
 }
+
+// console.log(getSum(0, -1))
