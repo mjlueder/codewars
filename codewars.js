559bfe99c28ff4af84d1da63e34b34c45eb383b1
Mary Lueder <mlueder@gmail.com>
2023-03-16 12:11:19 -0400

    refactor with replace

--- codewars.js
+++ codewars.js
@@ -338,8 +338,13 @@ function addBinary(a,b) {
 
 // Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 
+
+// function removeExclamationMarks(s) {
+//   return s.split('').filter(c => (c !== '!')).join('')
+// }
+
 function removeExclamationMarks(s) {
-  return s.split('').filter(c => (c !== '!')).join('')
+  return s.replace(/!/g, '')
 }
 
 console.log(removeExclamationMarks('!test!'));
