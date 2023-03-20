47b6469855caacbe08c84fc3a9e013b970b1ac5e
Mary Lueder <mlueder@gmail.com>
2023-03-20 14:34:11 -0400

    setup and attempt unique in order problem. Challenges with testing for empty array

--- codewars.js
+++ codewars.js
@@ -431,6 +431,35 @@ function cockroachSpeed(s) {
   return Math.floor(s * 1000 / 36)
 }
 
-console.log(cockroachSpeed('1.08'))
+// console.log(cockroachSpeed('1.08'))
 
 // speed * 100,000cm/km * 1hr/3600 seconds
+
+
+
+// Unique in Order
+
+// Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+// For example:
+
+// uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+// uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+// uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+var uniqueInOrder=function(iterable){
+  console.log(typeof iterable);
+  if (typeof iterable === 'object' && iterable.length === 0) return iterable
+  if (typeof iterable === 'string') iterable = iterable.split('')
+  console.log(iterable);
+  let newArr = []
+  newArr.push(iterable[0])
+  for (let i = 1; i < iterable.length; i++) {
+    if (iterable[i] !== iterable[i - 1]) {
+      newArr.push(iterable[i])
+    } 
+  }
+  return newArr
+}
+
+console.log(uniqueInOrder([1,2,2,3,3]))
