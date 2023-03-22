c6d3f6d80a841643b8b174754a77d68c7070255c
Mary Lueder <mlueder@gmail.com>
2023-03-22 12:39:08 -0400

    solve population growth problem

--- codewars.js
+++ codewars.js
@@ -512,3 +512,13 @@ function findNb(m) {
 // Note:
 // Don't forget to convert the percent parameter as a percentage in the body of your function: if the parameter percent is 2 you have to convert it to 0.02.
 
+function nbYear(p0, percent, aug, p) {
+  let years = 0
+  while (p0 < p) {
+    p0 = Math.floor(p0 + p0 * (percent/100) + aug)
+    years++
+  }
+  return years
+}
+
+// console.log(nbYear(1500000, 2.5, 10000, 2000000));
