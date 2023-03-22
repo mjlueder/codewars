1c8bd41fd667bb111dd9d3762511a3dba843db4e
Mary Lueder <mlueder@gmail.com>
2023-03-22 12:24:07 -0400

    setup population growth problem

--- codewars.js
+++ codewars.js
@@ -495,3 +495,20 @@ function findNb(m) {
 // console.log(findNb(1071225));
 // console.log(findNb(-2))
 // console.log(findNb(1071228));
+
+
+
+// Population Growth
+
+// In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
+
+// the function nb_year should return n number of entire years needed to get a population greater or equal to p.
+
+// aug is an integer, percent a positive or null floating number, p0 and p are positive integers (> 0)
+
+// Examples:
+// nb_year(1500, 5, 100, 5000) -> 15
+// nb_year(1500000, 2.5, 10000, 2000000) -> 10
+// Note:
+// Don't forget to convert the percent parameter as a percentage in the body of your function: if the parameter percent is 2 you have to convert it to 0.02.
+
