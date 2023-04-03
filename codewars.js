95705a1a2fe7e1b233d10b58f1263c3f716550dd
Mary Lueder <mlueder@gmail.com>
2023-04-03 16:02:31 -0400

    setup expanded number problem

--- codewars.js
+++ codewars.js
@@ -871,3 +871,18 @@ function abbrevName(name){
 
 // console.log(abbrevName('Sam Harris'))
 
+// Expanded form number
+
+// Write Number in Expanded Form
+// You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+// expandedForm(12); // Should return '10 + 2'
+// expandedForm(42); // Should return '40 + 2'
+// expandedForm(70304); // Should return '70000 + 300 + 4'
+// NOTE: All numbers will be whole numbers greater than 0.
+
+function expandedForm(num) {
+  
+}
+
+console.log(expandedForm(70304))
