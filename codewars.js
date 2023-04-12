f5d78ccc277023cbba67decf4884c4f7110525ff
Mary Lueder <mlueder@gmail.com>
2023-04-12 13:41:27 -0400

    solve

--- codewars.js
+++ codewars.js
@@ -1122,7 +1122,14 @@ function getMiddle(s){
 // Output: true
 
 var isIsomorphic = function(s, t) {
-    
+  for (let i = 0; i < s.length; i++) {
+    const a = s.indexOf(s[i]);
+    const b = t.indexOf(t[i]);
+    if (t[a] !== t[i] || s[b] !== s[i]) {
+      return false;
+    };
+  };
+  return true;
 };
 
 // for loop: replace each
