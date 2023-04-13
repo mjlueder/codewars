a89d6adadb1732f6c24391121ae3018081db0135
Mary Lueder <mlueder@gmail.com>
2023-04-13 13:45:20 -0400

    set up languages problem

--- codewars.js
+++ codewars.js
@@ -1167,4 +1167,42 @@ var isSubsequence = function(s, t) {
   return true;
 };
 
-console.log(isSubsequence('abc', 'ahbgdc'));
+// Solved with help
+// console.log(isSubsequence('abc', 'ahbgdc'));
+
+// Languages
+
+// The Task
+// Think of a way to store the languages as a database (eg an object). The languages are listed below so you can copy and paste!
+// Write a 'welcome' function that takes a parameter 'language' (always a string), and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input.
+// The Database
+
+// Possible invalid inputs include:
+
+// IP_ADDRESS_INVALID - not a valid ipv4 or ipv6 ip address
+// IP_ADDRESS_NOT_FOUND - ip address not in the database
+// IP_ADDRESS_REQUIRED - no ip address was supplied
+
+const languages = {
+  english: 'Welcome',
+  czech: 'Vitejte',
+  danish: 'Velkomst',
+  dutch: 'Welkom',
+  estonian: 'Tere tulemast',
+  finnish: 'Tervetuloa',
+  flemish: 'Welgekomen',
+  french: 'Bienvenue',
+  german: 'Willkommen',
+  irish: 'Failte',
+  italian: 'Benvenuto',
+  latvian: 'Gaidits',
+  lithuanian: 'Laukiamas',
+  polish: 'Witamy',
+  spanish: 'Bienvenido',
+  swedish: 'Valkommen',
+  welsh: 'Croeso'
+}
+
+function greet(language) {
+
+}
