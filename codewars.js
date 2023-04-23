22238ae5f87e83184c455c89918e742128dab7da
Mary Lueder <mlueder@gmail.com>
2023-04-23 15:23:05 -0400

    set up integer sum problem

--- codewars.js
+++ codewars.js
@@ -1324,3 +1324,15 @@ const rps = (p1, p2) => {
     return 'Player 2 won!'
   }
 };
+
+// sum of 2 lowest integers
+
+// Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+// [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+function sumTwoSmallestNumbers(numbers) {  
+  //Code here
+}
