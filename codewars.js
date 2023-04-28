87328f43626220a05a2e3f7452dd0c06173db033
Mary Lueder <mlueder@gmail.com>
2023-04-28 13:23:48 -0400

    set up and solve terminal move problem

--- codewars.js
+++ codewars.js
@@ -1388,5 +1388,17 @@ function narcissistic(value) {
   return sum === value
 }
 
-console.log(narcissistic(153));
+// console.log(narcissistic(153));
+
+// Terminal game move function
+// In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+
+// Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+
+// Example:
+// move(3, 6) should equal 15
+
+function move (position, roll) {
+  return position + roll * 2
+}
 
