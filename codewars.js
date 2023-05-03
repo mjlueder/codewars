4c7fc429df8e36d8586e9b7dcf1e03df3f1f9cd2
Mary Lueder <mlueder@gmail.com>
2023-05-03 12:31:41 -0400

    set up rental care problem

--- codewars.js
+++ codewars.js
@@ -1447,3 +1447,10 @@ function rowSumOddNumbers(n) {
 
 // console.log(rowSumOddNumbers(2));
 
+// Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+
+// Write a code that gives out the total amount for different days(d).
+
+function rentalCarCost(d) {
+  
+}
