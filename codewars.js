f148b948a85ede3204b27c991825b0a9d44b2e30
Mary Lueder <mlueder@gmail.com>
2023-05-03 12:33:28 -0400

    solve

--- codewars.js
+++ codewars.js
@@ -1452,5 +1452,11 @@ function rowSumOddNumbers(n) {
 // Write a code that gives out the total amount for different days(d).
 
 function rentalCarCost(d) {
-  
+  if (d >= 7) {
+    return d * 40 - 50
+  } else if (d >= 3) {
+    return d * 40 - 20
+  } else {
+    return d * 40
+  }
 }
