fc4f179014a065a8354a071285c6b3b31cd6f50a
Mary Lueder <mlueder@gmail.com>
2023-05-05 17:12:09 -0400

    set up remove vowels problem

--- codewars.js
+++ codewars.js
@@ -1500,3 +1500,11 @@ var reverseList = function(head) {
   }
   return prevNode;
 };
+
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+function disemvowel(str) {
+  return str;
+}
