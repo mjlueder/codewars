677697ed47d98a021f8b60f37747cda6f127f2a3
Mary Lueder <mlueder@gmail.com>
2023-05-09 11:30:40 -0400

    set up linked list cycle problem

--- codewars.js
+++ codewars.js
@@ -1628,4 +1628,20 @@ function decrypt(encryptedText, n) {
   return encryptedText
 }
 
-console.log(decrypt("32104", 2));
+// console.log(decrypt("32104", 2));
+
+// Linked List Cycle
+
+// Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+
+// Do not modify the linked list.
+
+// Input: head = [3,2,0,-4], pos = 1
+// Output: tail connects to node index 1
+// Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+var detectCycle = function(head) {
+  
+};
