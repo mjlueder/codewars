1637322cfc7ef7be44d08593eed740625e16c3da
Mary Lueder <mlueder@gmail.com>
2023-05-09 11:32:46 -0400

    set up sheep problem

--- codewars.js
+++ codewars.js
@@ -1661,3 +1661,23 @@ var detectCycle = function(head) {
   }
   return null
 };
+
+// Sheep Counting
+
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+// For example,
+
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+// The correct answer would be 17.
+
+// Hint: Don't forget to check for bad values like null/undefined
+
+function countSheeps(arrayOfSheep) {
+  // TODO May the force be with you
+}
