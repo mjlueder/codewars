633e5a65082d723c21ed25bf9369172a213a649f
Mary Lueder <mlueder@gmail.com>
2023-05-10 14:58:32 -0400

    solve (w google)

--- codewars.js
+++ codewars.js
@@ -1675,5 +1675,21 @@ function between(a, b) {
 // Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
 
 var maxSequence = function(arr){
-  // ...
+  if (arr.length === 0) return 0; // Empty array case
+  let maxSum = 0;
+  let currentSum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    currentSum += arr[i];
+    if (currentSum > maxSum) {
+      maxSum = currentSum;
+    }
+    if (currentSum < 0) {
+      currentSum = 0;
+    }
+  }
+
+  return maxSum;
 }
+
+// console.log(maxSequence());
