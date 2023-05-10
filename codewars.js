90b39b327bc0792783c9e4d6b84371570a0a38b3
Mary Lueder <mlueder@gmail.com>
2023-05-10 12:41:07 -0400

    set up array int problem

--- codewars.js
+++ codewars.js
@@ -1715,4 +1715,16 @@ function tribonacci(signature,n){
   return signature
 }
 
-console.log(tribonacci([1, 2, 3], 4));
+// console.log(tribonacci([1, 2, 3], 4));
+
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+// For example:
+
+// a = 1
+// b = 4
+// --> [1, 2, 3, 4]
+
+function between(a, b) {
+  // your code here
+}
