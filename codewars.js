7d0a29aa11d8ff4d00e175697119cfddea9b4b91
Mary Lueder <mlueder@gmail.com>
2023-05-19 12:45:03 -0400

    solve

--- codewars.js
+++ codewars.js
@@ -1867,5 +1867,18 @@ var longestPalindrome = function(s) {
 // Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
 
 var maxSequence = function(arr){
-  // ...
+  let maxSum = 0
+  let currentSum = 0
+  for (let i = 0; i < arr.length; i++) {
+    currentSum += arr[i]
+    if (currentSum < 0) {
+      currentSum = 0
+    }
+    if (currentSum > maxSum) {
+      maxSum = currentSum
+    }
+  }
+  return maxSum
 }
+
+// console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
