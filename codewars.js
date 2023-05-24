6e12be837794993ed052a5457ec6fecc42b4f988
Mary Lueder <mlueder@gmail.com>
2023-05-24 13:12:12 -0400

    solve

--- codewars.js
+++ codewars.js
@@ -1960,5 +1960,10 @@ function booleanToString(b){
 // * url = "https://www.cnet.com"                -> domain name = "cnet"
 
 function domainName(url){
-  //your code here
+  url = url.replace('http://', '')
+  url = url.replace('https://', '')
+  url = url.replace('www.', '')
+  return url.split('.')[0]
 }
+
+console.log(domainName("http://github.com/carbonfive/raygun"));
