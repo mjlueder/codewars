fa51d62ce7c26d6f095f1fed1afe6b593cf10e0a
Mary Lueder <mlueder@gmail.com>
2023-06-17 14:33:31 -0400

    solve

--- codewars.js
+++ codewars.js
@@ -2192,5 +2192,23 @@ function nameShuffler(str){
 // consecutive strings : follow one after another without an interruption
 
 function longestConsec(strarr, k) {
+  const n = strarr.length;
   
+  if (n === 0 || k > n || k <= 0) {
+    return '';
+  }
+  
+  let longestString = '';
+  
+  for (let i = 0; i < n - k + 1; i++) {
+    const currentString = strarr.slice(i, i + k).join('');
+    
+    if (currentString.length > longestString.length) {
+      longestString = currentString;
+    }
+  }
+  
+  return longestString;
 }
+
+console.log(longestConsec(["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], 2));
