8721494a23d2fdb30fc722fef2347f52fa12f966
Mary Lueder <mlueder@gmail.com>
2023-06-19 15:38:57 -0400

    set up isogram problem

--- codewars.js
+++ codewars.js
@@ -2211,4 +2211,18 @@ function longestConsec(strarr, k) {
   return longestString;
 }
 
-console.log(longestConsec(["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], 2));
+// console.log(longestConsec(["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], 2));
+
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+// Example: (Input --> Output)
+
+// "Dermatoglyphics" --> true "aba" --> false "moOse" --> false (ignore letter case)
+
+// isIsogram "Dermatoglyphics" = true
+// isIsogram "moose" = false
+// isIsogram "aba" = false
+
+function isIsogram(str){
+  //...
+}
