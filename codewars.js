d3d3d3d7ad1f3bbf5d615449ee2e3bdaa228d3a5
Mary Lueder <mlueder@gmail.com>
2023-06-22 10:34:46 -0400

    set up double char problem

--- codewars.js
+++ codewars.js
@@ -2239,3 +2239,14 @@ function isIsogram(str){
 
   return new Set(str.toLowerCase()).size == str.length
 }
+
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+// Examples (Input -> Output):
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+
+function doubleChar(str) {
+  // Your code here
+}
