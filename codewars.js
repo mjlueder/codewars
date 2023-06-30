31ecba6c9e2301a3793baa8adb8167791b435c53
Mary Lueder <mlueder@gmail.com>
2023-06-30 14:11:26 -0400

    set up printer error problem

--- codewars.js
+++ codewars.js
@@ -2344,4 +2344,21 @@ function DNAtoRNA(dna) {
   return dna.replace(/T/g, 'U')
 }
 
-console.log(DNAtoRNA('GCAT'));
+// console.log(DNAtoRNA('GCAT'));
+
+// a-m is correct. Anything else is an error.
+
+// You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+
+// The string has a length greater or equal to one and contains only letters from ato z.
+
+// Examples:
+// s="aaabbbbhaijjjm"
+// printer_error(s) => "0/14"
+
+// s="aaaxbbbbyyhwawiwjjjwwm"
+// printer_error(s) => "8/22"
+
+function printerError(s) {
+  // your code
+}
