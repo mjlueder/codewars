3b5765f815323548a6c3e3038b512351d33c9dff
Mary Lueder <mlueder@gmail.com>
2023-07-24 10:26:35 -0400

    set up greeting function

--- codewars.js
+++ codewars.js
@@ -2415,3 +2415,9 @@ function returnsFive() {
 function twiceAsOld(dadYearsOld, sonYearsOld) {
   return Math.abs((2 * sonYearsOld) - dadYearsOld)
 }
+
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+function greet(name){
+  //your code here
+}
