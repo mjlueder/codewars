0c79cee05f3fafa2406fb6b663f6562dc72ba120
Mary Lueder <mlueder@gmail.com>
2023-07-26 14:40:08 -0400

    solve

--- codewars.js
+++ codewars.js
@@ -2464,5 +2464,7 @@ function findEvenIndex(arr){
 // Since we cannot define keywords in Javascript (well, at least I don't know how to do it), your task is to define a function xor(a, b) where a and b are the two expressions to be evaluated. Your xor function should have the behaviour described above, returning true if exactly one of the two expressions evaluate to true, false otherwise.
 
 function xor(a, b) {
-  // TODO: Program Me
+  return (a && !b || b && !a)
 }
+
+// console.log(xor(false, 0));
