1442cc43c521eb653567547beb5c92dda2d9c177
Mary Lueder <mlueder@gmail.com>
2023-09-18 19:22:17 -0400

    set up diamond problem

--- codewars.js
+++ codewars.js
@@ -2531,3 +2531,23 @@ function solution(nums){
 
 // console.log(solution([1, 2, 10, 50, 5]));
 // console.log(solution(null));
+
+// Jamie is a programmer, and James' girlfriend. She likes diamonds, and wants a diamond string from James. Since James doesn't know how to make this happen, he needs your help.
+
+// Task
+// You need to return a string that looks like a diamond shape when printed on the screen, using asterisk (*) characters. Trailing spaces should be removed, and every line must be terminated with a newline character (\n).
+
+// Return null/nil/None/... if the input is an even number or negative, as it is not possible to print a diamond of even or negative size.
+
+// Examples
+// A size 3 diamond:
+
+//  *
+// ***
+//  *
+// ...which would appear as a string of " *\n***\n *\n"
+
+function diamond(n){
+  return '';
+}
+
