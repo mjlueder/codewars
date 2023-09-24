f4ff2ab7bd584cde39d79236bbfa315c09b87247
Mary Lueder <mlueder@gmail.com>
2023-09-23 23:12:56 -0400

    set up accum problem

--- codewars.js
+++ codewars.js
@@ -2579,4 +2579,18 @@ function repeatStr (n, s) {
   return s.repeat(n);
 }
 
-console.log(repeatStr(3, "tlou is the best *"));
+// console.log(repeatStr(3, "tlou is the best *"));
+
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+function accum(s) {
+
+}
+
+console.log(accum('tlou'));
