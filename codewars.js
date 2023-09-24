d652d156ccd4fc9025ce0de384e3b410ff4f1cde
Mary Lueder <mlueder@gmail.com>
2023-09-23 23:08:11 -0400

    set up string repeat problem

--- codewars.js
+++ codewars.js
@@ -2565,6 +2565,18 @@ function diamond(n){
   return di.join('')
 }
 
-console.log(diamond(5));
+// console.log(diamond(5));
 // console.log("  *\n ***\n*****\n ***\n  *\n");
 // console.log('*'.repeat(2));
+
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+function repeatStr (n, s) {
+  
+}
+
+console.log();
