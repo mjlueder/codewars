46978f5076547aa1d9170ed17851dd2d4f3140a5
Mary Lueder <mlueder@gmail.com>
2023-10-04 15:38:54 -0400

    solve

--- codewars.js
+++ codewars.js
@@ -2716,5 +2716,5 @@ var max = function(list){
 // Complete the function which converts a binary number (given as a string) to a decimal number.
 
 function binToDec(bin) {
-  // TODO
+  return parseInt(bin, 2)
 }
