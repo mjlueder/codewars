c65005548212dbcd9a88330a867dde6c33c5fc39
Mary Lueder <mlueder@gmail.com>
2023-11-02 17:13:45 -0400

    set up summation problem

--- codewars.js
+++ codewars.js
@@ -2846,3 +2846,16 @@ function makeNegative(num) {
     return num * -1
   }
 }
+
+// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+// For example (Input -> Output):
+
+// 2 -> 3 (1 + 2)
+// 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+let summation = function (num) {
+
+}
+
+console.log(summation(4));
