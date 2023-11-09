44a0e91b44d540cbeb79b934bc64b261047c851e
Mary Lueder <mlueder@gmail.com>
2023-11-09 17:55:20 -0500

    solve

--- codewars.js
+++ codewars.js
@@ -2894,5 +2894,5 @@ function simpleMultiplication(number) {
 // Complete the function which converts hex number (given as a string) to a decimal number.
 
 function hexToDec(hexString){
-  //your code here
+  return parseInt(hexString, 16)
 }
